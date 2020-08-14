4cd8a2797dc87335a4a06a36d8c923d198985c27
sivarajak <sivarajak@gmail.com>
2020-08-14 15:46:58 -0400

    updated the static files location

--- server.js
+++ server.js
@@ -23,14 +23,14 @@ app.use(forceSSL());
 
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/vChase'));
 
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/vChase/index.html'));
 });
 
 // Start the app by listening on the default
 // Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 4200);
